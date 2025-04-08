f9948d585fa7b6094f6268af7c15af8404d97dc0
unknown <jacobvestergaard@hotmail.dk>
2025-04-08 12:50:51 +0200

    added skilldata json example

---
+++ app/interfaces-ts/SkillData.ts
@@ -0,0 +1,11 @@
+export interface SkillData {
+    data: {
+        name: string;
+        totalTime: number;
+        streak: number;
+        TimeInfo: {
+            dato: string;
+            time: number;
+        }[]
+    }
+}
